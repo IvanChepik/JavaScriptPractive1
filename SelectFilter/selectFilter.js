a2252b8426808c6aa91f0df3751fe01925829b27
Ivan Chepik <barbudas.barbudass@gmail.com>
2019-08-05 02:22:43 +0300

    create skeleton

---
+++ SelectFilter/selectFilter.js
@@ -0,0 +1,82 @@
+var friends = [
+    {
+        name: 'Сэм',
+        gender: 'Мужской',
+        email: 'luisazamora@example.com',
+        favoriteFruit: 'Картофель'
+    },
+    {
+        name: 'Эмили',
+        gender: 'Женский',
+        email: 'example@example.com',
+        favoriteFruit: 'Яблоко'
+    },
+    {
+        name: 'Мэт',
+        gender: 'Мужской',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Яблоко'
+    },
+    {
+        name: 'Брэд',
+        gender: 'Мужской',
+        email: 'newtonwilliams@example.com',
+        favoriteFruit: 'Банан'
+    },
+    {
+        name: 'Шерри',
+        gender: 'Женский',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Картофель'
+    },
+    {
+        name: 'Керри',
+        gender: 'Женский',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Апельсин'
+    },
+    {
+        name: 'Стелла',
+        gender: 'Женский',
+        email: 'waltersguzman@example.com',
+        favoriteFruit: 'Картофель'
+    }
+];
+
+function select()
+{
+    var fields = [].slice.call(arguments);
+    return function()
+    {
+        
+    }
+}
+
+function filterIn()
+{
+
+}
+
+function cloneCollection()
+{
+
+}
+
+function cloneItem(item, properties)
+{
+    var newItem = {};
+
+    for (var i = 0; i<properties.length; i++)
+    {
+        newItem[properties[i]] = item[properties[i]]; 
+    }
+
+    return newItem;
+}
+
+function query(collection)
+{
+
+}
+
+
